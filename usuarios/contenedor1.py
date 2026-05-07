54201f85bd826575d1f336ce30698d712905c5ae
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-05-06 22:38:50 -0500

    feat: contenedor1.py

--- usuarios/contenedor1.py
+++ usuarios/contenedor1.py
@@ -2,40 +2,43 @@ import requests, boto3, pandas as pd
 import os
 
 MS1_URL = os.getenv("MS1_URL")
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 BUCKET = "peliculas-datalake"
 
-def fetch_todos_los_registros():
+def fetch_dump():
     try:
-        resp = requests.get(f"{MS1_URL}/usuarios_registros", timeout=30)
+        resp = requests.get(
+            f"{MS1_URL}/admin/dump",
+            auth=(ADMIN_EMAIL, ADMIN_PASSWORD),
+            timeout=300
+        )
         resp.raise_for_status()
         return resp.json()
     except requests.exceptions.RequestException as e:
-        print(f"❌ Error al conectar con la API: {e}")
+        print(f" Error al conectar con la API: {e}")
         return None
 
-data = fetch_todos_los_registros()
+data = fetch_dump()
 
 if not data:
-    print("⚠️ No se obtuvieron datos")
+    print("No se obtuvieron datos")
 else:
     s3 = boto3.client("s3")
 
     tablas = {
         "usuarios":         data.get("usuarios", []),
-        "listas":           data.get("listas", []),
-        "listas_peliculas": data.get("listas_peliculas", []),
+        "peliculas":        data.get("peliculas", []),
         "peliculas_vistas": data.get("peliculas_vistas", []),
     }
 
     for nombre, registros in tablas.items():
         if not registros:
-            print(f"⚠️  {nombre}: sin registros, se omite")
+            print(f"{nombre}: sin registros, se omite")
             continue
-
         df = pd.DataFrame(registros)
         local_path = f"/app/output/{nombre}.csv"
-        s3_key = f"usuarios/{nombre}.csv"
-
+        s3_key = f"usuarios/{nombre}/{nombre}.csv"
         df.to_csv(local_path, index=False)
         s3.upload_file(local_path, BUCKET, s3_key)
-        print(f"✅ {nombre}.csv → s3://{BUCKET}/{s3_key} ({len(df)} filas)")
+        print(f" {nombre}.csv → s3://{BUCKET}/{s3_key} ({len(df)} filas)")
